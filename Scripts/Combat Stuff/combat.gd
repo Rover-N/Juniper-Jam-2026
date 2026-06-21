03d6b594ec69d6b6c23bdde6c8e41243d376ac88
Noah <Weedenenoah@gmail.com>
2026-06-21 13:54:26 -0400

    Add enemy spawn logic

--- Scripts/Combat Stuff/combat.gd
+++ Scripts/Combat Stuff/combat.gd
@@ -5,5 +5,16 @@ extends Node2D
 @export var encounter_table: EncounterTable
 var enemies: Array[Enemy]
 
+func add_enemy():
+	var n = 0
+	var table_winner: EnemyType
+	var threshold = randi_range(0, encounter_table.total_weight)
+	for encounter in encounter_table.table:
+		while threshold > n:
+			n += encounter_table.table[encounter]
+		table_winner = encounter
+	enemies.append(Enemy.new(table_winner))
+
 func _ready() -> void:
-	pass
+	add_enemy()
+	print(enemies[0].attack)
